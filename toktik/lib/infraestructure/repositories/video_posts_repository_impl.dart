722109772e4d78f3fd5c3edf4765797a897c425a
yair91 <yair18nomas@gmail.com>
2024-04-15 12:20:28 -0600

    use of repository and datasources

--- toktik/lib/infraestructure/repositories/video_posts_repository_impl.dart
+++ toktik/lib/infraestructure/repositories/video_posts_repository_impl.dart
@@ -1,13 +1,11 @@
 import 'package:toktik/domain/datasourse/video_post_datasourse.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/domain/repositories/video_post_repository.dart';
-import 'package:toktik/infraestructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
 
-class LocalVideoRepositoryImpl implements VideoPostRepository {
+class VideoPostsRepositoryImpl implements VideoPostRepository {
   final VideoPostDatasourse videosDatasourse;
 
-  LocalVideoRepositoryImpl({required this.videosDatasourse});
+  VideoPostsRepositoryImpl({required this.videosDatasourse});
 
   @override
   Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
